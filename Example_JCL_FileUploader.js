8890b01469547b02c005995edd19b10c3f306536
Jacques Clement <jacques.clement@accenture.com>
2023-10-22 13:44:29 +0200

    Changed example parameters

--- Example_JCL_FileUploader.js
+++ Example_JCL_FileUploader.js
@@ -2,7 +2,7 @@
 
     // Create a probe to be sent to the MID Server
 
-    var jspr = new JavascriptProbe('M le Martien MID 1');
+    var jspr = new JavascriptProbe('imac_ubuntu');
 
     // Configure which MID Server Script Include and function to call
 
@@ -14,12 +14,12 @@
     jspr.addParameter('verbose', 'true');
     jspr.addParameter('instance', 'dev68475.service-now.com')
     jspr.addParameter('tableName', 'sys_data_source');
-    jspr.addParameter('documentId', 'b3fa22432f932010ab05e36ef699b6bc');
-    jspr.addParameter('fileLocation', '/servicenow/mlemartien_mid1/agent/upload');
-    jspr.addParameter('fileName', 'users.csv');
+    jspr.addParameter('documentId', '695a8c61970af110c8aff2a3f153af02');
+    jspr.addParameter('fileLocation', '/opt/servicenow/mid/agent/uploads');
+    jspr.addParameter('fileName', 'locations.csv');
 
     // Send the request to the MID Server
 
     jspr.create();
 
-}();
+})();
